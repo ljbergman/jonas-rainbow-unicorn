9bf20b2ac914ff6af3c59d7b6ff3a0da738db721
Jonas Bergman <ljbergman@outlook.com>
2023-01-16 11:43:43 +0100

    Test av objekt

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 "use strict";
 const arr = ["apa", "orangutang", "gorilla", "get", "giraff"];
 console.log(arr);
+let mittObjekt = {
+    x: 12,
+    y: "testtext",
+    z: true,
+};
+console.log(mittObjekt);
--- app.ts
+++ app.ts
@@ -4,3 +4,19 @@ const arr:string[] = ["apa", "orangutang", "gorilla", "get", "giraff"];
 
 console.log(arr);
 
+let mittObjekt: {
+
+ x: number,
+ y: string,
+ z: boolean,
+
+} = {
+
+   x: 12,
+   y: "testtext",
+   z: true,
+
+}
+
+console.log(mittObjekt);
+
